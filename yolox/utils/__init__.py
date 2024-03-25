a2695955b6b73c47295f6ed98779a2e39740df92
tenderribs <dc3dude@gmail.com>
2024-03-25 15:37:44 +0100

    remove redundant imports

--- yolox/utils/__init__.py
+++ yolox/utils/__init__.py
@@ -14,7 +14,3 @@ from .metric import *
 from .model_utils import *
 from .setup_env import *
 from .visualize import *
-from .visualize_object_pose import *
-from .visualize_human_pose import *
-from .object_pose_utils import *
-from .plots import *
